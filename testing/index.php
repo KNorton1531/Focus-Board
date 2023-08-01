c6cfff537c80c1736e579fc13497c27e07d83f57
KNorton1531 <127792500+KNorton1531@users.noreply.github.com>
2023-08-01 22:07:19 +0100

    Initial starting point

---
+++ testing/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+
+    <video autoplay muted loop id="myVideo">
+        <source src="https://lofico.nyc3.cdn.digitaloceanspaces.com/scenes/cottage/cottage-exterior-final.mp4" type="video/mp4">
+    </video>
+
+        <div class="scene">
+
+            <div id="dragContainer1">
+              <div class="draggable"></div>
+              <p onclick="deleteDraggable()">Delete other container</p>
+            </div>
+
+            <div id="dragContainer2">
+                <div class="draggable"></div>
+            </div>
+
+        </div>
+
+    </body>
+
+
+
+        <script src="js/plain-draggable.min.js"></script>
+        <script src="js/index.js" defer></script>
+    </body>
+</html>
---
+++ testing/js/index.js
@@ -0,0 +1,19 @@
+
+    var draggable1 = new PlainDraggable(document.getElementById('dragContainer1'),
+      {handle: document.querySelector('#dragContainer1 .draggable')});
+
+    var draggable2 = new PlainDraggable(document.getElementById('dragContainer2'),
+    {handle: document.querySelector('#dragContainer2 .draggable')});
+    
+
+
+  
+  function deleteDraggable(){
+    var draggable1 = document.getElementById('dragContainer2');
+
+    if (draggable1.style.display === "none") {
+      draggable1.style.display = "block";
+    } else {
+      draggable1.style.display = "none";
+    }
+  }
