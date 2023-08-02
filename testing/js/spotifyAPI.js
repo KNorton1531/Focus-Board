e471a3a04bd35cf96193dfabd382709c49104ca9
Kyle Norton <dexterandkyle@live.co.uk>
2023-08-02 16:24:35 +0100

    Update songs and save access token

--- testing/js/spotifyAPI.js
+++ testing/js/spotifyAPI.js
@@ -1,61 +1,79 @@
-  // Function to extract the access token from the URL
-  function getSpotifyAuthToken() {
+// Function to extract the access token from the URL
+function getSpotifyAuthToken() {
     const urlParams = new URLSearchParams(window.location.search);
     const authToken = urlParams.get('code');
     return authToken;
   }
-
-  let accessToken = '';
-  let refreshToken = '';
-
+  
+  function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+  
+  let accessToken = getCookie('spotifyAccessToken') || '';
+  let refreshToken = getCookie('spotifyRefreshToken') || '';
+  
+  if (!accessToken) {
+    fetchSpotifyToken().then(tokens => {
+      accessToken = tokens.accessToken;
+      refreshToken = tokens.refreshToken;
+      
+      document.cookie = 'spotifyAccessToken=' + accessToken;
+      document.cookie = 'spotifyRefreshToken=' + refreshToken;
+    
+      document.getElementById('spotifyContainer').removeChild(document.querySelector('p'));
+      
+      fetchCurrentlyPlaying(); // Called once immediately
+      setInterval(fetchCurrentlyPlaying, 10000);  // Then updates every 10 seconds
+    });
+  } else {
+    fetchCurrentlyPlaying(); // Called once immediately
+    setInterval(fetchCurrentlyPlaying, 10000);  // Then updates every 10 seconds
+  }
+  
   async function fetchSpotifyToken() {
     const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: 'Basic N2I5NmE2NjhmOGNmNGI4NDg4MTllYmVmMjg5YTIzMzY6MDk2ZDRhZWU4NTg0NDBhY2EzNzQ1MWVjZTU2N2IzMDA='
-      },
-      body: new URLSearchParams({
-        grant_type: 'authorization_code',
-        code: getSpotifyAuthToken(),
-        redirect_uri: 'http://nortonwebtesting.free.nf/?i=2'
-      })
-    };
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic N2I5NmE2NjhmOGNmNGI4NDg4MTllYmVmMjg5YTIzMzY6MDk2ZDRhZWU4NTg0NDBhY2EzNzQ1MWVjZTU2N2IzMDA='
+        },
+        body: new URLSearchParams({
+          grant_type: 'authorization_code',
+          code: getSpotifyAuthToken(),
+          redirect_uri: 'http://nortonwebtesting.free.nf/?i=2'
+        })
+      };
+  
+      try {
+            const response = await fetch('https://accounts.spotify.com/api/token', options);
+            const data = await response.json();
+            accessToken = data.access_token;
+            refreshToken = data.refresh_token;
+            return { accessToken, refreshToken };
+        } catch (err) {
+            return console.error(err);
+        }
 
-    try {
-          const response = await fetch('https://accounts.spotify.com/api/token', options);
-          const data = await response.json();
-          accessToken = data.access_token;
-          refreshToken = data.refresh_token;
-          return { accessToken, refreshToken };
-      } catch (err) {
-          return console.error(err);
-      }
   }
-
-// Call the fetchSpotifyToken function to perform the API request
-  fetchSpotifyToken().then(async (tokens) => {
+  
+  async function fetchCurrentlyPlaying() {
     const options = {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer ' + tokens.accessToken
+        Authorization: 'Bearer ' + accessToken
       }
     };
-
+  
     try {
       const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', options);
       const responseData = await response.json();
-      
-      // Store the response data into a variable
       const currentlyPlayingData = responseData;
-
-      // Now you can use the currentlyPlayingData variable as needed
       console.log(currentlyPlayingData.item.name);
-
-      // Access the first element with class "nowPlaying" and update its content
       document.getElementById("nowPlaying").innerHTML = currentlyPlayingData.item.name;
-
     } catch (err) {
       console.error(err);
     }
-  });
+  }
+  
--- testing/templates/spotifyContainer.php
+++ testing/templates/spotifyContainer.php
@@ -1,8 +1,8 @@
 <div id="spotifyContainer">
     <div class="draggable"></div>
-    <p>Spotify Container <a href="https://accounts.spotify.com/authorize?response_type=code&client_id=7b96a668f8cf4b848819ebef289a2336&redirect_uri=http%3A%2F%2Fnortonwebtesting.free.nf%2F%3Fi%3D2&scope=user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing">Link Spotify</a></p>
+    <p>Spotify Container <a id="nowPlayingContainer" href="https://accounts.spotify.com/authorize?response_type=code&client_id=7b96a668f8cf4b848819ebef289a2336&redirect_uri=http%3A%2F%2Fnortonwebtesting.free.nf%2F%3Fi%3D2&scope=user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing">Link Spotify</a></p>
 
     <h4>Currently playing:</h4><div id="nowPlaying"></div>
-
-
 </div>
+
+
