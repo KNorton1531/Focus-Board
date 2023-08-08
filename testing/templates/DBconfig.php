a312bd3c6723825e1abfb1cb4b806f251cadf95c
Knorton1531 <dexterandkyle@live.co.uk>
2023-08-08 22:12:32 +0100

    Switch to local details

--- testing/templates/DBconfig.php
+++ testing/templates/DBconfig.php
@@ -1,10 +1,20 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'sql212.infinityfree.com');
-define('DB_USERNAME', 'if0_34709976');
-define('DB_PASSWORD', 'zs9Q8FIOu57');
-define('DB_NAME', 'if0_34709976_focus_board');
+
+// LIVE Details
+// define('DB_SERVER', 'sql212.infinityfree.com');
+// define('DB_USERNAME', 'if0_34709976');
+// define('DB_PASSWORD', 'zs9Q8FIOu57');
+// define('DB_NAME', 'if0_34709976_focus_board');
+
+// LOCAL Details
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'focus_board');
+
+
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- testing/templates/countdowns.php
+++ testing/templates/countdowns.php
@@ -14,18 +14,18 @@
         <?php
 
             // LOCAL DETAILS
-            // $host = 'localhost';
-            // $db   = 'focus_board';
-            // $user = 'root';
-            // $pass = '';
-            // $charset = 'utf8mb4';
+            $host = 'localhost';
+            $db   = 'focus_board';
+            $user = 'root';
+            $pass = '';
+            $charset = 'utf8mb4';
 
             // LIVE DETAILS
-            $host = 'sql212.infinityfree.com';
-            $db   = 'if0_34709976_focus_board';
-            $user = 'if0_34709976';
-            $pass = 'zs9Q8FIOu57';
-            $charset = 'utf8mb4';
+            // $host = 'sql212.infinityfree.com';
+            // $db   = 'if0_34709976_focus_board';
+            // $user = 'if0_34709976';
+            // $pass = 'zs9Q8FIOu57';
+            // $charset = 'utf8mb4';
 
             $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
             $options = [
